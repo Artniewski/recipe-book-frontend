0a4ac3b4aa531ade5b058d670fb4f1a727a0a5d9
Artniewski <artniewski@gmail.com>
2023-05-20 14:28:04 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,18 +2,46 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import RecipeForm from './RecipeForm';
-import RecipeList from './RecipeList';
+import { Provider } from 'react-native-paper'
+
+import { theme } from './src/core/theme'
+import {
+  AuthLoadingScreen,
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ResetPasswordScreen,
+  Dashboard,
+  RecipeForm,
+  RecipeList
+} from './src/screens'
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="RecipeList">
-        <Stack.Screen name="RecipeForm" component={RecipeForm} />
-        <Stack.Screen name="RecipeList" component={RecipeList} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="AuthLoadingScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen
+            name="AuthLoadingScreen"
+            component={AuthLoadingScreen}
+          />
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen
+            name="ResetPasswordScreen"
+            component={ResetPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
 }
